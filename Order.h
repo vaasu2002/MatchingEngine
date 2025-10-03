f3283d5dc639abcd908e5bc884b95a9905e95229
vaasu2002 <vaasu.bisht2021@vitbhopal.ac.in>
2025-10-04 00:21:27 +0530

    feat: Added StopPrice

--- Order.h
+++ Order.h
@@ -16,15 +16,19 @@ namespace OrderEngine
     class Order{
     public:
         Order(Base::OrderId m_id, Base::Symbol  m_symbol, Base::OrderSide m_side,
-            Base::Quantity m_oty,Base::Price m_price)
+            Base::Quantity m_oty,Base::Price m_price,Base::Price stopPrice)
             : mId(m_id),
               mSymbol(std::move(m_symbol)),
               mSide(m_side),
-              mOty(m_oty),
+              mOty(m_oty), mType(),
               mOpenQty(m_oty),
               mPrice(m_price),
+              mStopPrice(stopPrice),
               mStatus(Base::OrderStatus::PENDING),
-              mCreatedAt(std::chrono::high_resolution_clock::now()){}
+              mCreatedAt(std::chrono::high_resolution_clock::now())
+        {
+        }
+
 
         Base::OrderId GetId() const
         {
@@ -71,6 +75,15 @@ namespace OrderEngine
             mStatus = orderStatus;
         }
 
+        Base::OrderType GetOrderType() const
+        {
+            return mType;
+        }
+
+        Base::Price GetStopPrice() const
+        {
+            return mStopPrice;
+        }
         std::string ToString() const
         {
             std::ostringstream oss;
@@ -88,16 +101,27 @@ namespace OrderEngine
             return oss.str();
         }
 
+        bool iSBuy() const { return GetSide() == Base::OrderSide::BUY; }
+        bool isSell() const { return GetSide() == Base::OrderSide::SELL; }
+        bool isMarket() const { return GetOrderType() == Base::OrderType::MARKET; }
+        bool isLimit() const { return GetOrderType() == Base::OrderType::LIMIT; }
+        bool isStop() const { return GetOrderType() == Base::OrderType::STOP || GetOrderType() == Base::OrderType::STOP_LIMIT; }
+        static bool isAllOrNone() { return false; }
+        void set_open_quantity(uint64_t uint64);
+        // bool isImmediateOrCancel() const { return time_in_force() == TimeInForce::IMMEDIATE_OR_CANCEL; }
+        // bool isFillOrKill() const { return time_in_force() == TimeInForce::FILL_OR_KILL; }
+
     private:
         Base::OrderId mId;
         Base::Symbol mSymbol;
         Base::OrderSide mSide;
         Base::Quantity mOty;
+        Base::OrderType mType;
         Base::Quantity mOpenQty;
         Base::Price mPrice;
         Base::OrderStatus mStatus;
         Base::Timestamp mCreatedAt;
-
+        Base::Price mStopPrice;
         static std::string OrderSideToString(Base::OrderSide side)
         {
             switch (side)
